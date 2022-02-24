ff807fa54491f6f75741d44e8c4eb5d7f90a4525
FJAimer <2987318064@qq.com>
2022-02-24 21:08:11 +0800

    '京东图书爬取1.1'

--- jdBooks/jdBooks/spiders/Jbooks.py
+++ jdBooks/jdBooks/spiders/Jbooks.py
@@ -1,4 +1,5 @@
 import scrapy
+from jdBooks.items import JdbooksItem
 
 
 class JbooksSpider(scrapy.Spider):
@@ -10,4 +11,10 @@ class JbooksSpider(scrapy.Spider):
 
     # 3、实现爬取逻辑
     def parse(self, response):
-        pass
+        # 获取所有图书大分类节点列表
+        big_node_list = response.xpath("//*[@id="booksort"]/div[2]/dl/dt/a")
+        # 循环遍历图书大分类
+        for big_node in big_node_list[:1]:
+            big_category = big_node.xpath("./text()").extract_first()
+            big_category_link = big_node.xpath("./@href").extract_first()
+            print(big_category,big_category_link)
